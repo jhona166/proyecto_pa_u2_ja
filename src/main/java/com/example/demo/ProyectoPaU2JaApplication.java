2ed4266b9c028c832a651c011bc416e19a219cc8
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2023-01-10 13:28:30 -0500

    Tarea 6

--- src/main/java/com/example/demo/ProyectoPaU2JaApplication.java
+++ src/main/java/com/example/demo/ProyectoPaU2JaApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.uce.modelo.Estudiante;
+import com.example.demo.uce.modelo.Profesor;
 import com.example.demo.uce.service.IEstudianteService;
+import com.example.demo.uce.service.IProfesorService;
 
 @SpringBootApplication
 public class ProyectoPaU2JaApplication implements CommandLineRunner{
 	@Autowired
 	private IEstudianteService estudianteService;
+	@Autowired
+	private IProfesorService profesorService;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoPaU2JaApplication.class, args);
 	}
@@ -19,16 +22,25 @@ public class ProyectoPaU2JaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Estudiante estu = new Estudiante();
-		Estudiante estu1 = new Estudiante();
+		 Profesor prof = new Profesor();
+		 
+		 prof.setNombre("Pablo");
+		 prof.setApellido("Zaldumbide");
+		 prof.setCedula("1234567890");
+		 prof.setDireccion("San Carlos");
+		 prof.setGenero("M");
+		 prof.setCuidad("Quito");
+		 this.profesorService.agregar(prof);
+		
+		 //Estudiante estu = new Estudiante();
+		//Estudiante estu1 = new Estudiante();
 		//estu.setId(9);
-		estu.setNombre("Karol");
-		estu.setApellido("Analuisa");
-		estu.setCedula("1727501512");
-		estu.setCuidad("Quito");
-		estu.setGenero("F");
-	
-		this.estudianteService.agregar(estu);
+		//estu.setNombre("Karol");
+		//estu.setApellido("Analuisa");
+		//estu.setCedula("1727501512");
+		//estu.setCuidad("Quito");
+		//estu.setGenero("M");
+		//this.estudianteService.agregar(estu);
 		//estu.setCuidad("Guayaquil");
 		//this.estudianteService.modificar(estu);
 	}
