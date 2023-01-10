a062e0c74fbe805a0288381a612effa51dbbeec8
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2023-01-09 19:45:43 -0500

    Taller 19

--- src/main/java/com/example/demo/uce/repository/EstudianteRepoImpl.java
+++ src/main/java/com/example/demo/uce/repository/EstudianteRepoImpl.java
@@ -6,8 +6,10 @@ import com.example.demo.uce.modelo.Estudiante;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 @Repository
+@Transactional
 public class EstudianteRepoImpl implements IEstudianteRepo{
 	//Pto principal para interatuar con la BD
 	//Interfaz: EntityManager
---
+++ src/main/java/com/example/demo/uce/service/EstudianteServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.uce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.uce.modelo.Estudiante;
+import com.example.demo.uce.repository.IEstudianteRepo;
+
+@Service
+public class EstudianteServiceImpl implements IEstudianteService {
+	@Autowired
+	private IEstudianteRepo estudianteRepo;
+	@Override
+	public void agregar(Estudiante estudiante) {
+		// TODO Auto-generated method stub
+		this.estudianteRepo.insertar(estudiante);
+	}
+
+	@Override
+	public void modificar(Estudiante estudiante) {
+		// TODO Auto-generated method stub
+		this.estudianteRepo.actualizar(estudiante);
+	}
+
+	
+
+	@Override
+	public void eliminar(Integer id) {
+		// TODO Auto-generated method stub
+		this.estudianteRepo.eliminar(id);
+	}
+
+	@Override
+	public Estudiante buscar(Integer id) {
+		// TODO Auto-generated method stub
+		return this.estudianteRepo.buscar(id);
+	}
+
+
+
+}
